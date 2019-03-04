4fec7c8e4852524aea07ff7643305850496c6ef1
Kevan Stannard <kevan.stannard@ansarada.com>
2019-03-04 22:28:16 +1100

    Complete Functor

--- src/Course/Functor.hs
+++ src/Course/Functor.hs
@@ -10,6 +10,15 @@ import Course.Optional
 import Course.List
 import qualified Prelude as P(fmap)
 
+
+{-
+*****************************************************
+*                                                   *
+*   A Functor is just a way to go from f a -> f b   *
+*                                                   *
+*****************************************************
+-}
+
 -- | All instances of the `Functor` type-class must satisfy two laws. These laws
 -- are not checked by the compiler. These laws are given as:
 --
@@ -41,8 +50,8 @@ instance Functor ExactlyOne where
     (a -> b)
     -> ExactlyOne a
     -> ExactlyOne b
-  (<$>) =
-    error "todo: Course.Functor (<$>)#instance ExactlyOne"
+  (<$>) f =
+    (ExactlyOne . f . runExactlyOne)
 
 -- | Maps a function on the List functor.
 --
@@ -57,7 +66,17 @@ instance Functor List where
     -> List a
     -> List b
   (<$>) =
-    error "todo: Course.Functor (<$>)#instance List"
+    map
+
+{-
+Note "browse" command:
+
+  >> :browse Course.List
+
+map works over lists
+fmap works over anything
+
+-}
 
 -- | Maps a function on the Optional functor.
 --
@@ -72,12 +91,14 @@ instance Functor Optional where
     -> Optional a
     -> Optional b
   (<$>) =
-    error "todo: Course.Functor (<$>)#instance Optional"
+    mapOptional
 
 -- | Maps a function on the reader ((->) t) functor.
 --
 -- >>> ((+1) <$> (*2)) 8
 -- 17
+
+{-
 instance Functor ((->) t) where
   (<$>) ::
     (a -> b)
@@ -85,6 +106,60 @@ instance Functor ((->) t) where
     -> ((->) t b)
   (<$>) =
     error "todo: Course.Functor (<$>)#((->) t)"
+-}
+
+instance Functor ((->) t) where
+  (<$>) ::
+    (a -> b)      -- This is: f :: a -> b
+    -> (t -> a)   -- This is: g :: t -> a
+    -> (t -> b)   -- This is: f (g t) :: t -> a -> b
+  -- (<$>) f g =
+  --   f . g
+  (<$>) =
+    (.)
+    
+{-
+Why is this called a reader?
+
+  Perhaps because it's explicitly declared to read a "t"?
+
+Note:
+
+  The functor for functions is composition.
+
+Reminder:
+
+  Int -> Bool
+  
+is the same as:
+  
+  (->) Int Bool
+  
+This is a function that takes an Int and returns a Bool
+
+Consider the Functor type. Another way of writing this:
+
+  instance Functor f where
+    (<$>) :: (a -> b) -> (f a) -> (f b)
+
+But here, f is:
+
+  (->) t
+
+Which is a function that takes in a "t".
+
+Replacing the f, we have:
+
+  instance Functor ((->) t) where
+    (<$>) :: (a -> b) -> ((->) t a) -> ((->) t b)
+
+Which is the same as:
+
+  instance Functor ((->) t) where
+    (<$>) :: (a -> b) -> (t -> a) -> (t -> b)
+
+-}
+
 
 -- | Anonymous map. Maps a constant value on a functor.
 --
@@ -99,8 +174,47 @@ instance Functor ((->) t) where
   a
   -> f b
   -> f a
-(<$) =
-  error "todo: Course.Functor#(<$)"
+-- (<$) a b = (<$>) (const a) b
+-- (<$) a = (<$>) (const a)
+(<$) = (<$>) . const
+
+{-
+
+Reminder:
+
+  const :: a -> b -> a
+
+What's happening here:
+
+  [1] "a" is just a simple value
+  [2] "f b" is b lifted into f. We DO care about f but we don't care about "b"
+  [3] "f a" is the result we want.
+
+  We essentially need to lift the "a" from [1] into the "f" from [2].
+
+  But we don't know what "f" is.
+
+  To modifiy a value inside "f", we can use FMAP, i.e. <$>.
+
+  For example:
+
+    >> (<$>) (\x -> x + 1) (Full 3)
+    Full 4
+
+  Now, suppose we want to throw away the value inside Full,
+  and make it a constant value.
+
+    >> (<$>) (\_ -> 7) (Full 3)
+    Full 7
+
+  But (\_ -> 7) is the same as (const 7), so we can write:
+
+    >> (<$>) (const 7) (Full 3)
+    Full 7
+
+  Which is the pattern in the solution.
+
+-}
 
 -- | Anonymous map producing unit value.
 --
@@ -120,7 +234,12 @@ void ::
   f a
   -> f ()
 void =
-  error "todo: Course.Functor#void"
+  (<$) ()
+
+{-
+Here we want to map a constant value onto a functor.
+In this case it's just the unit value.
+-}
 
 -----------------------
 -- SUPPORT LIBRARIES --
